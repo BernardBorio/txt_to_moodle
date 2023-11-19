2d60892d8182b7faa9de95f188f237961d598c9e
Bernard996 <bernardborio@gmail.com>
2023-11-19 23:20:44 +0100

    Minor fix

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ line_counter = 0
 sep_counter = 0
 inSpecificFeedback = False
 inGenericFeedback = False
+error = False
 
 separator = True
 for line in input_file:
@@ -31,6 +32,7 @@ for line in input_file:
 
             if not line[-1].isdigit():  # check if there's the number of correct answers
                 print(f"Error, number of correct answers missing at the line: {line_counter}")
+                error = True
                 output_file.close()
                 os.remove("domande.txt")
                 break
@@ -43,6 +45,7 @@ for line in input_file:
 
         else:
             print(f"Error, missing separator before line: {line_counter}")
+            error = True
             output_file.close()
             os.remove("domande.txt")
             break
@@ -89,7 +92,8 @@ for line in input_file:
         elif answer == 'F':
             output_file.write(f"\tFALSE\n")
         else:
-            print()
+            print("Error, wrong answer format")
+            error = True
 
     elif line[0] == '%':                # end of question case
         inSpecificFeedback = False
@@ -101,15 +105,16 @@ for line in input_file:
 
     else:
         if len(line.strip()) > 0:       # case of formatting error, only if not empty line
-            print(f"Error formatting the line file {line_counter}\n\t{line}")
+            print(f"Error: missing symbol indicator at line {line_counter}\n\t{line}")
+            error = True
             output_file.close()
             os.remove("domande.txt")
             break
 
-
-if sep_counter != counter:
-    print(f"You missed the last separator")
-    output_file.close()
-    os.remove("domande.txt")
-else:
-    print(f"You just created {counter} questions")
+if not error:
+    if sep_counter != counter:
+        print(f"You missed the last separator")
+        output_file.close()
+        os.remove("domande.txt")
+    else:
+        print(f"You just created {counter} questions")
